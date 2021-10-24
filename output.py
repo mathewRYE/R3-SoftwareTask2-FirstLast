6bf989cf0faebf9cb4b6d5f553451648a8b17a6f
mathewRYE <92241587+mathewRYE@users.noreply.github.com>
2021-10-24 17:36:41 -0400

    Updated .py files
    These are my updated python files for my output and input. I added code to implement the output for the differential steering.

--- output.py
+++ output.py
@@ -3,6 +3,13 @@ HOST = "127.0.0.1"
 PORT = 8080
 speed = 0
 direction = ""
+#dictionary that converts the direction inputted, into a set of directions for all wheels while moving using differential steering.
+motorconvert = {
+   "forward" : "ffff",
+   "left" : "rrff",
+   "backward" : 'rrrr',
+   "right" : "ffrr",
+}
 
 
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
@@ -12,23 +19,27 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
     print("listening")
     conn, addr =s.accept()
     with conn:
-       print('Connected by', addr)
-       while True:
+      print('Connected by', addr)
+      while True:
         #decodes the data upon receiving it from client
-           data = conn.recv(1024).decode("utf-8")
-           if not data:
-               break
+         data = conn.recv(1024).decode("utf-8")
+         if not data:
+            break
             #splits the data into type and value
-           data = data.split()
+         data = data.split()
             # if type is s, change the value of speed
-           if data[0] == "s":
-               speed = data[1]
+         if data[0] == "s":
+            speed = data[1]
             # if the type is d, change the value of direction
-           elif data[0] == "d":
-               direction = data[1]
+         elif data[0] == "d":
+            direction = data[1]
+            #variables representing each of the motors and their direction/speed values
+            m1 = motorconvert[direction][0]
+            m2 = motorconvert[direction][1]
+            m3 = motorconvert[direction][2]
+            m4 = motorconvert[direction][3]
         #prints the current speed and direction values following keyboard input
-           print(f"Current Speed: {speed} Current Direction: {direction}")
-           
+         print(f"[{m1}{speed}][{m2}{speed}][{m3}{speed}][{m4}{speed}]")
 
 
 
